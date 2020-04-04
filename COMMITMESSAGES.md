d00888b00d0a5f3b37e25de05240736cc632bda7
Wolfang Pfretzschner <wolfgangp@live.com>
2020-04-04 17:41:18 -0400

    2 backend routes and controller

--- COMMITMESSAGES.md
+++ COMMITMESSAGES.md
@@ -1,1 +1,3 @@
 1 - create backend with "rails new backend --api" and frontend with "npx react-native init frontend"
+
+2 - in backend: add routes, generate controller and model, add controller functions, 
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Todo.create(title: "Buy toilet paper", isComplete: false)
+Todo.create(title: "Buy more toilet paper", isComplete: false)
+Todo.create(title: "Buy more and more toilet paper", isComplete: false)
+Todo.create(title: "Buy all the toilet paper", isComplete: false)
